e7f2069121c811476a09194dc024e7faa56534ed
andersonstack <andersong.pereiracruz@gmail.com>
2025-11-23 13:32:51 -0300

    style: add favicon.ico

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={`${poppins.variable} w-full h-full  overflow-hidden`}>
         <Provider>{children}</Provider>
       </body>
